af8ef87cb7fa98a27f915ab6fea9c9a2b5ba18be
Shreyansh22485 <119041890+Shreyansh22485@users.noreply.github.com>
2025-02-15 12:43:14 +0530

    Generating insights

--- middleware.js
+++ middleware.js
@@ -10,10 +10,10 @@ const isProtectedRoute = createRouteMatcher([
 ]);
 
 export default clerkMiddleware(async(auth,req)=>{
-    const {userId} = auth();
+    const {userId} = await auth();
 
     if(!userId && isProtectedRoute(req)){
-        const {redirectToSignIn} = auth();
+        const {redirectToSignIn} = await auth();
         return redirectToSignIn()
     }
     return NextResponse.next();
